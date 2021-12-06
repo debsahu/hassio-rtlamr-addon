700292ac17f5f74db12acfe25cfe04a90590e727
Debashish Sahu <debashish.sahu@gmail.com>
2021-12-06 14:03:47 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # hassio-rtlamr-addon
+
+A HASSIO addon to run [RTLAMR](https://github.com/bemasher/rtlamr) and [RTLAMR_COLLECT](https://github.com/bemasher/rtlamr-collect) on RTL-SDR dongles (RTL2832U). Idea is to read data from meters (water/gas/electric) and import data into InfluxDB database.
+
+## 1. Install RTL-SDR
+
+- Follow instructions on [OSMOCOM](https://osmocom.org/projects/rtl-sdr/wiki/Rtl-sdr) to install manually
+- Install using apt-get
+```
+$ sudo apt-get update
+$ sudo apt-get install rtl-sdr
+```
+- Make sure that `lsmod | grep dvb` does not show anything or may require a reboot
+
+## 2. Run rtl_tcp as a service at boot
+
+- Create TCP service `sudo vi /etc/systemd/system/rtl_tcp.service`
+```
+[Unit]
+Description=Software Defined Radio TCP Server
+Wants=network.target
+After=network.target
+
+[Service]
+ExecStart=/usr/local/bin/rtl_tcp
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+```
+- Enable service
+```
+$ sudo systemctl enable rtl_tcp.service
+```
+- Start rtl_tcp
+```
+$ sudo systemctl start rtl_tcp.service
+```
+
+## 3. Run RTLAMR & RTLAMR-collect
+
+- Add appropiate values to config and run this add on
---
+++ rtlamr/rootfs/etc/cont-init.d/rtlenvcvreate.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bashio
+
+declare -a options
+declare RTLAMR_FORMAT
+declare RTLAMR_MSGTYPE
+declare RTLAMR_CENTERFREQ
+declare RTLAMR_FILTERID
+declare COLLECT_INFLUXDB_HOSTNAME
+declare COLLECT_INFLUXDB_DATABASE
+declare COLLECT_INFLUXDB_TOKEN
+declare COLLECT_INFLUXDB_ORG
+declare COLLECT_INFLUXDB_BUCKET
+declare COLLECT_INFLUXDB_MEASUREMENT
+declare COLLECT_STRICTIDM
+
+RTLAMR_FORMAT = $(bashio::config 'RTLAMR_FORMAT')
+RTLAMR_MSGTYPE = $(bashio::config 'RTLAMR_MSGTYPE')
+RTLAMR_CENTERFREQ = $(bashio::config 'RTLAMR_CENTERFREQ')
+RTLAMR_FILTERID = $(bashio::config 'RTLAMR_FILTERID')
+COLLECT_INFLUXDB_HOSTNAME = $(bashio::config 'COLLECT_INFLUXDB_HOSTNAME')
+COLLECT_INFLUXDB_DATABASE = $(bashio::config 'COLLECT_INFLUXDB_DATABASE')
+COLLECT_INFLUXDB_TOKEN = $(bashio::config 'COLLECT_INFLUXDB_TOKEN')
+COLLECT_INFLUXDB_ORG = $(bashio::config 'COLLECT_INFLUXDB_ORG')
+COLLECT_INFLUXDB_BUCKET = $(bashio::config 'COLLECT_INFLUXDB_BUCKET')
+COLLECT_INFLUXDB_MEASUREMENT = $(bashio::config 'COLLECT_INFLUXDB_MEASUREMENT')
+COLLECT_STRICTIDM = $(bashio::config 'COLLECT_STRICTIDM')
+
+if ! bashio::fs.file_exists "/config/rtlamr-collect.env"; then
+    echo -e "" > /config/rtlamr-collect.env
+    echo -e "RTLAMR_FORMAT=${RTLAMR_FORMAT}\n" >> /config/rtlamr-collect.env
+    echo -e "RTLAMR_MSGTYPE=${RTLAMR_MSGTYPE}\n" >> /config/rtlamr-collect.env
+    echo -e "RTLAMR_CENTERFREQ=${RTLAMR_CENTERFREQ}\n" >> /config/rtlamr-collect.env
+    if bashio::config.has_value 'RTLAMR_FILTERID'; then
+        echo -e "RTLAMR_FILTERID=${RTLAMR_FILTERID}\n" >> /config/rtlamr-collect.env
+    fi
+    echo -e "COLLECT_INFLUXDB_HOSTNAME=${COLLECT_INFLUXDB_HOSTNAME}\n" >> /config/rtlamr-collect.env
+    echo -e "COLLECT_INFLUXDB_DATABASE=${COLLECT_INFLUXDB_DATABASE}\n" >> /config/rtlamr-collect.env
+    echo -e "COLLECT_INFLUXDB_TOKEN=${COLLECT_INFLUXDB_TOKEN}\n" >> /config/rtlamr-collect.env
+    echo -e "COLLECT_INFLUXDB_ORG=${COLLECT_INFLUXDB_ORG}\n" >> /config/rtlamr-collect.env
+    echo -e "COLLECT_INFLUXDB_BUCKET=${COLLECT_INFLUXDB_BUCKET}\n" >> /config/rtlamr-collect.env
+    echo -e "COLLECT_INFLUXDB_MEASUREMENT=${COLLECT_INFLUXDB_MEASUREMENT}\n" >> /config/rtlamr-collect.env
+    echo -e "COLLECT_STRICTIDM=${COLLECT_STRICTIDM}\n" >> /config/rtlamr-collect.env
+fi
